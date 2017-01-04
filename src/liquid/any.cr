c016a22a55b54438295f31e30d6f189612a7ebad
Arnaud Fernandés <arnaud.fernandes@tech-magister.com>
2017-01-04 09:31:21 +0100

    Remove AnyHash

--- src/liquid/any.cr
+++ src/liquid/any.cr
@@ -28,24 +28,4 @@ end
 
 module Liquid
   alias Any = JSON::Any
-
-  struct AnyHash
-    getter raw
-
-    def initialize
-      @raw = Hash(String, JSON::Type).new
-    end
-
-    {% for x in %w(String Int64 Bool Float64) %}
-    def []=(key : String, value : {{x.id}})
-      @raw[key] = value.as(JSON::Type)
-    end
-    {% end %}
-
-    def []=(key : String, value : Array)
-    end
-
-    def []=(key : String, value : Hash)
-    end
-  end
 end
